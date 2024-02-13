dd6ee4ae8e5a7323a5206078cdeeef71a62a99b2
190101101 <190101101@mail.ru>
2024-02-13 12:19:48 +0400

    1.1

---
+++ resources/resource/main/js/radio.js
@@ -0,0 +1,867 @@
+const elements = [
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  {
+    title: "Baba Radyo",
+    type: "Arabesk",
+    url: "http://37.247.98.7:80/;stream.mp3",
+  },
+  {
+    title: "Best Fm",
+    type: "Türkçe Pop",
+    url: "http://46.20.7.125/listen.pls",
+  },
+  {
+    title: "İstanbul Fm",
+    type: "Türkçe Pop",
+    url: "http://45.32.154.169:9300/;",
+  },
+  {
+    title: "Pal Nostalji",
+    type: "Türkçe Klasik",
+    url: "http://shoutcast.radyogrup.com:1010/;",
+  },
+  
+];
+
+
+const radio = document.querySelector('#radio');
+const player = document.querySelector('#player');
+
+elements.map((element, index) => {
+  index+=1;
+	radio.innerHTML += `<div class="col-lg-3 col-md-4 col-sm-6">
+												<a class="card" data-src="${element.url}">${index} - [${element.type}] - ${element.title}</a>
+                      </div>`;
+})
+
+radio.addEventListener('click', async (e) => {
+  if (!e.target.classList.contains("card")) return;
+
+  // Yeni ses dosyasının yüklenmesi bekleniyor
+  await new Promise((resolve, reject) => {
+    player.setAttribute('src', e.target.getAttribute('data-src'));
+    player.oncanplay = resolve; // Ses dosyası yüklendiğinde resolve olacak
+    player.onerror = reject;    // Hata durumunda reject olacak
+  });
+
+  // Ses dosyası yüklendikten sonra otomatik olarak çalmaya başla
+  player.play();
+
+  // Diğer işlemleri devam ettir
+  radio.querySelectorAll('.card').forEach((card) => card.classList.remove('active'));
+  e.target.classList.add('active');
+});
